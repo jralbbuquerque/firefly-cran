fc1e9d9586e96b2908628402774d5ee1505e734d
Francisco Júnior <jralbbuquerque@gmail.com>
2020-04-22 16:54:06 -0300

    Initialize repository

---
+++ main.m
@@ -0,0 +1,6 @@
+run('./util/fa');
+
+fprintf('\n\tThe illustrated graph (Figure 2) corresponds to Firefly`s \n')
+fprintf('\tbest results in comparison to BAT and PSO to solve the problem addressed.\n\n')
+
+open('./plots/graph_comparate.fig');
